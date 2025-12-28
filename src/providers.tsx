231ea951a85600d98d26b3b70e3f80a96ecce51d
Francisco <francisco@mondey.co>
2025-12-28 23:58:08 +0100

    add comments for local proxy configuration in PostHog initialization

--- src/providers.tsx
+++ src/providers.tsx
@@ -7,6 +7,11 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY as string, {
+      // If proxying enabled create the call this way, works only on localhost
+      /* 
+      api_host: '/ingest', // Points to your local proxy
+      ui_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.posthog.com',
+      */
       api_host:
         import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for all users
