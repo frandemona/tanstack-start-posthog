efd7f7cc003d1f3ec0d37683ed52597549983c03
Francisco <francisco@mondey.co>
2025-12-28 23:25:35 +0100

    Initial Commit

---
+++ src/providers.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  const [hydrated, setHydrated] = useState(false)
+
+  useEffect(() => {
+    posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for all users
+      defaults: '2025-11-30',
+    })
+
+    setHydrated(true)
+  }, [])
+
+  if (!hydrated) return <>{children}</>
+  return <PHProvider client={posthog}>{children}</PHProvider>
+}
